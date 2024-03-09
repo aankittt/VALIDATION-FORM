7924c86bc8dd74e09063260efadd2ffdfb8db524
ANKIT TRIPATHI <117687642+aankittt@users.noreply.github.com>
2024-03-10 01:06:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function clearErrors(){
 
 }
 function seterror(id, error){
-    //sets error inside tag of id 
     element = document.getElementById(id);
     element.getElementsByClassName('formerror')[0].innerHTML = error;
 
@@ -18,8 +17,6 @@ function seterror(id, error){
 function validateForm(){
     var returnval = true;
     clearErrors();
-
-    //perform validation and if validation fails, set the value of returnval to false
     var name = document.forms['myForm']["fname"].value;
     if (name.length<5){
         seterror("name", "*Length of name is too short");
@@ -46,7 +43,6 @@ function validateForm(){
     var password = document.forms['myForm']["fpass"].value;
     if (password.length < 6){
 
-        // Quiz: create a logic to allow only those passwords which contain atleast one letter, one number and one special character and one uppercase letter
         seterror("pass", "*Password should be atleast 6 characters long!");
         returnval = false;
     }
